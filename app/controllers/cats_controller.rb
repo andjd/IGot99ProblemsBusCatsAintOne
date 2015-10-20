bda777920607dbdc541e1cd2b7a9c1547174842a
AndrewJohn <this.is@email.fake>
2015-10-20 14:37:29 -0400

    Create new and index views.

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -12,8 +12,23 @@ class CatsController < ApplicationController
     render :show
   end
 
-  # def new
-  #
-  # end
+  def new
+    render :new
+  end
+
+  def create
+    @cat = Cat.new(cat_params)
+
+    if @cat.save
+      redirect_to(controller: :cats, action: :show, id: @cat.id)
+    else
+      render json: "Save failed.", status: :unprocessable_entity
+    end
+  end
+
+  private
+    def cat_params
+      params[:cat].permit(:birthdate, :color, :name, :sex, :description)
+    end
 
 end
